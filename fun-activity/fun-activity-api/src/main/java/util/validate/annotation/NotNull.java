639c8f25973a4871117cbce4e6f45db0a2d5918f
diwa <di@diwas-MacBook-Pro.local>
2016-08-01 11:09:34 +0800

    add common

---
+++ fun-activity/fun-activity-api/src/main/java/util/validate/annotation/NotNull.java
@@ -0,0 +1,12 @@
+package util.validate.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by di on 19/7/2016.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NotNull {
+}
